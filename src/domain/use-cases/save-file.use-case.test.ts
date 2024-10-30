7acba90792c7c8b281877451a238c1396cc09b54
fernandevx <ortizfernando05@gmail.com>
2024-10-30 14:25:28 -0600

    test: add testing for save file use case

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -7,12 +7,17 @@ describe("SaveFileUseCase", () => {
     fileDestination: "custom-outputs/file-destination",
     fileName: "custom-table-name",
   };
-  
-  const customFilePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`
-  
+
+  const customFilePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`;
+
   afterEach(() => {
     if (fs.existsSync("outputs")) fs.rmSync("outputs", { recursive: true });
-    if (fs.existsSync("custom-outputs")) fs.rmSync("custom-outputs", { recursive: true });
+    if (fs.existsSync("custom-outputs"))
+      fs.rmSync("custom-outputs", { recursive: true });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
   test("should save file with default values", () => {
@@ -41,5 +46,33 @@ describe("SaveFileUseCase", () => {
     expect(result).toBeTruthy();
     expect(fileExists).toBeTruthy();
     expect(fileContent).toBe(fileContent);
+  });
+
+  test("should return false if directory could not be created", () => {
+    const saveFile = new SaveFile();
+    const mkdirSyncMock = jest.spyOn(fs, "mkdirSync").mockImplementation(() => {
+      throw new Error("This is a custom error message from testing");
+    });
+
+    const result = saveFile.execute(customOptions);
+
+    expect(result).toBe(false);
+
+    mkdirSyncMock.mockRestore();
+  });
+
+  test("should return false if file could not be created", () => {
+    const saveFile = new SaveFile();
+    const writeFileSyncMock = jest
+      .spyOn(fs, "writeFileSync")
+      .mockImplementation(() => {
+        throw new Error("This is a custom error message from testing");
+      });
+
+    const result = saveFile.execute({ fileContent: "test content" });
+
+    expect(result).toBeFalsy();
+
+    writeFileSyncMock.mockRestore();
   });
 });
